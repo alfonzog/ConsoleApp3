72a0e8939a4858041f784bda5b630185b2d2633a
Jose Armando Alfonzo Garcia <jalfonzo@uninter.edu.mx>
2023-05-19 20:29:01 -0600

    Add project files.

---
+++ ConsoleApp3/Ciudadano.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+  internal class Ciudadano
+  {
+    public string curp { get; }
+    public string nombre { get; }
+    public string apePat { get; }
+    public string apeMat { get; }
+    public int edad { get; }
+
+    public Ciudadano(string curp, string nombre, string apePat, string apeMat, int edad)
+    {
+      this.curp = curp;
+      this.nombre = nombre;
+      this.apePat = apePat;
+      this.apeMat = apeMat;
+      this.edad = edad;
+    }
+  }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConsoleApp3;
+
+Ciudadano c;
+
+List<Ciudadano> ciudadanos = new List<Ciudadano>();
+c = new("ADSFASD4532", "AMERICA", "LOPEZ", "PEREZ", 10);
+ciudadanos.Add(c);
+
+var menoresEdad = from menor in ciudadanos
+                  where menor.edad < 18
+                  select menor;
+
+foreach(Ciudadano ciudadano in menoresEdad)
+{
+  Console.WriteLine("CURP: {0} NOMBRE: {1} EDAD {2}", 
+    ciudadano.curp, ciudadano.nombre, ciudadano.edad);
+}
+
+
